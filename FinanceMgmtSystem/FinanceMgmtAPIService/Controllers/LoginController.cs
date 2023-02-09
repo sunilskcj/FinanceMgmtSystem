cb9a2af69deaf110cb085d0d86ff4a752d6dc8a8
sunilskcj <sunilskcj@gmail.com>
2023-02-09 22:28:24 +0530

    setup init

---
+++ FinanceMgmtSystem/FinanceMgmtAPIService/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using FinanceMgmtReporsitory.Interface;
+using FinanceMgmtReporsitory.Models;
+using FinanceMgmtReporsitory.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceMgmtAPIService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private ILoginDao LoginDao;
+        public LoginController(ILoginDao LoginDao)
+        {
+            this.LoginDao = LoginDao;
+        }
+
+        [HttpPost]
+        [Route("admin")]
+        public IActionResult LoginUser(LoginModel loginMode)
+        {
+            bool isAuthenticate = LoginDao.IsAuthenticatedUser(loginMode);
+            if (!isAuthenticate) return this.NotFound("Invalid Username or password");
+            return this.Ok(new
+            {
+                StatusCode = 201,
+
+                Data = loginMode
+            }) ;
+        }
+
+        [HttpGet]
+        [Route("{UserID}")]
+        public IActionResult UserDetailbyID(string UserID)
+        {
+            UserDetail userDetail = LoginDao.GetbyUserId(UserID);
+            return this.Ok(userDetail);
+        }
+    }
+}
