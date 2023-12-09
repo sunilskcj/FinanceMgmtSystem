77aacdd42c6de41e56c73fddacf1820bfa6861e1
sunilskcj <sunilskcj@gmail.com>
2023-12-09 14:56:22 +0530

    Abstract

---
+++ finance_mgmt_server/finance_mgmt_Repository/DataAccessLayer/Abstraction/ILogin.cs
@@ -0,0 +1,15 @@
+﻿using finance_mgmt_Repository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finance_mgmt_Repository.DataAccessLayer.Abstraction
+{
+    public interface ILogin
+    {
+        public int isUserExist(User i);
+        public int updatePassword();
+    }
+}
