3c6975b3a937154eb1bb12885818ac2e88eca780
sunilskcj <sunilskcj@gmail.com>
2024-01-30 12:54:06 +0530

    Navbar changes

--- finaince_mgmt_client/src/Components/Navbar/Navbar.js
+++ finaince_mgmt_client/src/Components/Navbar/Navbar.js
@@ -4,55 +4,52 @@ import './Navbar.css';
 
 
 function Navbar(){
-
+  let nbsp = "\u00A0"
     return(
       
-      <div class="adbd">
-      <nav class="navbar navbar-light " >
-         <div class="container-fluid">
-           
-           <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-             <span class="navbar-toggler-icon"></span>
-           </button>
-           <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" style={{backgroundColor:"#F8F8F8"}}>
-             <div class="offcanvas-header">
-               <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Finance Manager</h5>
-               <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-             </div>
-             <div class="offcanvas-body" >
-               <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-                 <li class="nav-item">
-                   <a class="nav-link " aria-current="page" href="user/expenseView">DashBoard</a>
-                 </li>
-                 
-                <li   class="nav-item">
-                  <a class="nav-link " aria-current="page" href="user/profile">Profile</a>
-                </li>
-                <li   class="nav-item">
-                <a class="nav-link " aria-current="page" href="/">View Table</a>
-              </li>
-                <li class="nav-item">
-                  <a class="nav-link " aria-current="page" href="addExpense">Add Expenses</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link " aria-current="page" href="user/changepassword">Change Password</a>
-                </li>
-                 <li class="nav-item">
-                   <a class="nav-link" href="/">Logout</a>
-                 </li>
-                 
-                   </ul>
-                 
-              
-              
-             </div>
-           </div>
-           <a class="navbar-brand" href="#">Finance Manager</a>
-           </div> 
-         
-       </nav>
+      <div class="d-flex flex-column flex-shrink-0 p-3 text-white " style={{height:'100vh',width:'300px',position:'-webkit-sticky',position:'sticky',top:'0',backgroundColor:'#fff'}}>
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
        
-       </div>
+        <span class="fs-5">Finance Management</span>
+      </a>
+      <hr />
+      <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item">
+         <Link to='/'  class="nav-link text-dark" aria-current="page">
+         
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='/view' class="nav-link text-dark">
+          
+            Dashboard
+          </Link>
+        </li>
+        <li>
+        <Link to='/add' class="nav-link text-dark">
+          
+          Add Expense
+        </Link>
+      </li>
+     
+    </ul>
+    <hr />
+    <div class="dropdown">
+      <a href="#" class="d-flex align-items-center text-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+        <img src="https://www.hdwallpapers.in/download/mac_miller_is_wearing_blue_jacket_and_black_goggles_in_a_blur_background_hd_celebrities-HD.jpg" alt="" width="32" height="32" class="rounded-circle me-2" />
+        <strong>Profile</strong>
+      </a>
+      <ul class="dropdown-menu dropdown-menu-light text-small shadow" aria-labelledby="dropdownUser1">
+        <li><a class="dropdown-item" href="#">New project...</a></li>
+        <li><a class="dropdown-item" href="#">Settings</a></li>
+        <li><a class="dropdown-item" href="#">Profile</a></li>
+        <li><hr class="dropdown-divider" /></li>
+        <li><a class="dropdown-item" href="#">Sign out</a></li>
+      </ul>
+    </div>
+    </div>
+    
        );
 }
 
