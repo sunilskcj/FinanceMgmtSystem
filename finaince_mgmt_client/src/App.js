fb52b324760038f15640a40ddc89745e61b3e2ef
sunilskcj <sunilskcj@gmail.com>
2024-02-02 12:54:06 +0530

    user v0

--- finaince_mgmt_client/src/App.js
+++ finaince_mgmt_client/src/App.js
@@ -2,25 +2,18 @@
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import './App.css';
 import Create_User from './Components/create-user/create-user';
-import Navbar from './Components/Navbar/Navbar';
+
 import Home from './Components/Home/Home';
 import Footer from './Components/Footer/Footer';
 import LoginUser from './Components/LoginUser/LoginUser';
 import User from './Components/User/User';
-import AddExpenses from './Components/User/AddExpenses/AddExpenses';
+
 
 function App() {
   let nbsp = "\u00A0"
   return (
   <div className='App'>
-  <BrowserRouter>
-<Navbar />
-<Routes>
-<Route exact path='/view' element={<User />}/>
-<Route path='/add' element={<AddExpenses />}/>
-</Routes>
-</BrowserRouter>
-
+  <User />
 
   </div>
     
