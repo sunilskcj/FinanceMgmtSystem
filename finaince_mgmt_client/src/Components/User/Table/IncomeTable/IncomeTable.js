c06cccf1182885925717757c084b3b44222a148f
sunilskcj <sunilskcj@gmail.com>
2024-02-04 12:54:06 +0530

    Income Table v1

--- finaince_mgmt_client/src/Components/User/Table/IncomeTable/IncomeTable.js
+++ finaince_mgmt_client/src/Components/User/Table/IncomeTable/IncomeTable.js
@@ -2,6 +2,7 @@ import Table from 'react-bootstrap/Table';
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "./IncomeTable.css"
+import moment from 'moment';
 
 function IncomeTable() {
   let nbsp = "\u00A0"
@@ -29,7 +30,7 @@ function IncomeTable() {
       if(post == null) return null;
   return (
     <div className='incomeTable'>
-  
+  <br />
    
    <table>
       <thead>
@@ -48,7 +49,7 @@ function IncomeTable() {
          <td>{exp.expenses}</td>
          <td>{exp.amount}</td>
          <td>{exp.notes}</td>
-         <td>{exp.expenseDate}</td>
+         <td style={{fontSize:'14px'}}>{moment(exp.expenseDate).format('DD-MMM-YYYY')}</td>
          
          <td><svg onClick={(e)=>handleOpen(e)} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-three-dots-vertical" viewBox="0 0 16 16">
          <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
