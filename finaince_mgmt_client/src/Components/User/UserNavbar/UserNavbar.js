c1b9e5320dd6e9a18f561d53b5741d6a0de64708
sunilskcj <sunilskcj@gmail.com>
2024-02-17 12:54:06 +0530

    Nav change

--- finaince_mgmt_client/src/Components/User/UserNavbar/UserNavbar.js
+++ finaince_mgmt_client/src/Components/User/UserNavbar/UserNavbar.js
@@ -7,27 +7,27 @@ function UserNavbar(){
   let nbsp = "\u00A0"
     return(
       
-      <div class="d-flex flex-column flex-shrink-0 p-3 text-white " style={{height:'100vh',width:'300px',position:'-webkit-sticky',position:'sticky',top:'0',backgroundColor:'#fff'}}>
-      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+      <div className="d-flex flex-column flex-shrink-0 p-3 text-white " style={{height:'100vh',width:'300px',position:'-webkit-sticky',position:'sticky',top:'0',backgroundColor:'#fff'}}>
+      <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
        
-        <span class="fs-5">Finance Management</span>
+        <span className="fs-5">Finance Management</span>
       </a>
       <hr />
-      <ul class="nav nav-pills flex-column mb-auto">
-        <li class="nav-item">
-         <Link to='/'  class="nav-link text-dark" aria-current="page">
+      <ul className="nav nav-pills flex-column mb-auto">
+        <li className="nav-item">
+         <Link to='/'  className="nav-link text-dark" aria-current="page">
          
             Home
           </Link>
         </li>
         <li>
-          <Link to='/view' class="nav-link text-dark">
+          <Link to='/view' className="nav-link text-dark">
           
             Dashboard
           </Link>
         </li>
         <li>
-        <Link to='/add' class="nav-link text-dark">
+        <Link to='/add' className="nav-link text-dark">
           
           Add Expense
         </Link>
@@ -35,17 +35,17 @@ function UserNavbar(){
      
     </ul>
     <hr />
-    <div class="dropdown">
-      <a href="#" class="d-flex align-items-center text-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-        <img src="https://www.hdwallpapers.in/download/mac_miller_is_wearing_blue_jacket_and_black_goggles_in_a_blur_background_hd_celebrities-HD.jpg" alt="" width="32" height="32" class="rounded-circle me-2" />
+    <div className="dropdown">
+      <a href="#" className="d-flex align-items-center text-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+        <img src="https://www.hdwallpapers.in/download/mac_miller_is_wearing_blue_jacket_and_black_goggles_in_a_blur_background_hd_celebrities-HD.jpg" alt="" width="32" height="32" className="rounded-circle me-2" />
         <strong>Profile</strong>
       </a>
-      <ul class="dropdown-menu dropdown-menu-light text-small shadow" aria-labelledby="dropdownUser1">
-        <li><a class="dropdown-item" href="#">New project...</a></li>
-        <li><a class="dropdown-item" href="#">Settings</a></li>
-        <li><a class="dropdown-item" href="#">Profile</a></li>
-        <li><hr class="dropdown-divider" /></li>
-        <li><a class="dropdown-item" href="#">Sign out</a></li>
+      <ul className="dropdown-menu dropdown-menu-light text-small shadow" aria-labelledby="dropdownUser1">
+        <li><a className="dropdown-item" href="#">New project...</a></li>
+        <li><a className="dropdown-item" href="#">Settings</a></li>
+        <li><a className="dropdown-item" href="#">Profile</a></li>
+        <li><hr className="dropdown-divider" /></li>
+        <li><a className="dropdown-item" href="#">Sign out</a></li>
       </ul>
     </div>
     </div>
