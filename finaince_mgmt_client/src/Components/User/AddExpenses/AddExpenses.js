e0a9114e48633ca8e00ecc0dc06685fa9a2ea3b3
sunilskcj <sunilskcj@gmail.com>
2024-02-21 21:16:56 +0530

    added category in Add new page

--- finaince_mgmt_client/src/Components/User/AddExpenses/AddExpenses.js
+++ finaince_mgmt_client/src/Components/User/AddExpenses/AddExpenses.js
@@ -37,31 +37,31 @@ function AddExpenses(){
           <form onSubmit={(e)=>Submit(e)}>
           <div className="form-row">
             <div className="form-group">
-              <label for="expenses">Name</label>
+              <label htmlFor="expenses">Name</label>
               <input onChange={(e)=> handle(e)} value={data.expenses} type="text" className="form-control" id="expenses"  />
             </div>
             <div className="form-group">
-              <label for="Account">Account</label>
+              <label htmlFor="Account">Account</label>
               <input onChange={(e)=> handle(e)} value={data.accountId} type="text" className="form-control" id="accountsId"  />
             </div>
             <div className="form-group">
-            <label for="Amount">Amount</label>
-            <input onChange={(e)=> handle(e)} value={data.amount} type="number" className="form-control" id="amount"  />
+            <label htmlFor="category">Category</label>
+            <input onChange={(e)=> handle(e)} value={data.category} type="text" className="form-control" id="category"  />
           </div>
           <div className="form-group">
-            <label for="expenseType">Expense Type</label>
+            <label htmlFor="expenseType">Expense Type</label>
             <select onChange={(e) => handle(e)} id="expenseType" className="form-control">
             <option value="0">Income</option>
             <option value="1" >Expense</option>
-            <option selected value="">Null</option>
+            <option defaultValue="">Null</option>
             </select>
           </div>
           <div className="form-group">
-            <label for="Notes">Notes</label>
+            <label htmlFor="Notes">Notes</label>
             <input onChange={(e)=> handle(e)} value={data.notes} type="text" className="form-control" id="notes"  />
           </div>
           <div className="form-group">
-            <label for="Date">Date</label>
+            <label htmlFor="Date">Date</label>
             <input onChange={(e)=> handle(e)} value={data.expenseDate} type="date"  className="form-control" id="expenseDate"  />
           </div>
           </div>
