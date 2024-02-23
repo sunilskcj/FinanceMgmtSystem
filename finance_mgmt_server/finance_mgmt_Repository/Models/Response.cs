7c49d89ebfedc480890b941cc2728298f2a3a434
sunilskcj <sunilskcj@gmail.com>
2024-02-23 12:54:06 +0530

    Response model

---
+++ finance_mgmt_server/finance_mgmt_Repository/Models/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finance_mgmt_Repository.Models
+{
+    public class Response
+    {
+        public int? status;
+        public List<Expenditure>? data ;
+        public string? message ;
+    }
+}
