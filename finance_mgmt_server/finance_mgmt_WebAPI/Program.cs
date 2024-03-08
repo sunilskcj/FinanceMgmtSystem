d6f11be9e6e75d88e3bad701f75474b786ae105e
sunilskcj <sunilskcj@gmail.com>
2024-03-08 12:54:06 +0530

    program

--- finance_mgmt_server/finance_mgmt_WebAPI/Program.cs
+++ finance_mgmt_server/finance_mgmt_WebAPI/Program.cs
@@ -1,7 +1,13 @@
 using finance_mgmt_Repository.DataAccessLayer.Abstraction;
 using finance_mgmt_Repository.DataAccessLayer.Implementation;
 using finance_mgmt_Repository.Models;
+using finance_mgmt_WebAPI.Authentication;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,10 +21,21 @@ Action<DbContextOptionsBuilder> action = (DbContextOptionsBuilder builder) =>
     builder.UseSqlServer("Server=SKCJPC;Initial Catalog=FinanceMgmtSystm;Integrated Security=True;TrustServerCertificate=True;");
 };
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IJwtTokenManager, JwtTokenManager>();
 builder.Services.AddDbContext<FinanceMgmtSystmContext>(action, ServiceLifetime.Scoped);
 builder.Services.AddTransient<ILogin, LoginDao > ();
 builder.Services.AddTransient<IAccounts, AccountsDao>();
 builder.Services.AddTransient<IExpenditure, ExpenditureDao>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration.GetSection("Jwt:Key").Value)),
+        ValidateIssuer=false,
+        ValidateAudience=false
+    };
+});
 
 builder.Services.AddCors();
 var app = builder.Build();
@@ -29,6 +46,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseAuthentication();
 app.UseAuthorization();
 app.UseCors(
               policyBuilder =>
