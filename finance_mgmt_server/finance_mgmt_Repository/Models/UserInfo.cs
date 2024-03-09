679b9d1ae7bfb331c5e390edeeaa0425363c7ac9
sunilskcj <sunilskcj@gmail.com>
2024-03-09 12:54:06 +0530

    userinfo

---
+++ finance_mgmt_server/finance_mgmt_Repository/Models/UserInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace finance_mgmt_Repository.Models
+{
+    
+        public enum Roles
+        {
+            Admin,
+            User
+        }
+    public class UserInfo
+    {
+        public int UserId { get; set; }
+        public string Password { get; set; }
+        public Roles Role { get; set; }
+    }
+}
