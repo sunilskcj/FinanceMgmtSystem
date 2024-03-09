d1a97f7eadd753e4d04ae380d81ce9b01eb4801b
sunilskcj <sunilskcj@gmail.com>
2024-03-09 12:54:06 +0530

    IncomeTable

---
+++ finance_mgmt_server/finance_mgmt_Repository/Models/IncomeTable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace finance_mgmt_Repository.Models;
+
+public partial class IncomeTable
+{
+    public string? Category { get; set; }
+
+    public decimal? NetAmount { get; set; }
+}
