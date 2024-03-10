c71369117d3d40682194d474e9afbd05ab714c3d
sunilskcj <sunilskcj@gmail.com>
2024-03-10 12:54:06 +0530

    ExpenseTable

---
+++ finance_mgmt_server/finance_mgmt_Repository/Models/ExpenseTable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace finance_mgmt_Repository.Models;
+
+public partial class ExpenseTable
+{
+    public string? Category { get; set; }
+
+    public decimal? NetAmount { get; set; }
+}
