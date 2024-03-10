0fbea5705ec50cbcae487a7a275930350bebffc8
sunilskcj <sunilskcj@gmail.com>
2024-03-10 12:54:06 +0530

    IncomeTablebyId

---
+++ finance_mgmt_server/finance_mgmt_Repository/Models/IncomeTablebyId.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace finance_mgmt_Repository.Models;
+
+public partial class IncomeTablebyId
+{
+    public string? Expenses { get; set; }
+
+    public decimal? Amount { get; set; }
+
+    public int? ExpenseType { get; set; }
+
+    public int? FreqType { get; set; }
+
+    public DateTime? ExpenseDate { get; set; }
+
+    public string? Notes { get; set; }
+
+    public int? AccountsId { get; set; }
+
+    public int ExpId { get; set; }
+
+    public string? Category { get; set; }
+
+    public int? UserId { get; set; }
+}
