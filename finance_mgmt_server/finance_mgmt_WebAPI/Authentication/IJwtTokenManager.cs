73234bc27b578f47ee03407dc3c80d9c9f32d7a4
sunilskcj <sunilskcj@gmail.com>
2024-03-11 12:54:06 +0530

    IJWT

---
+++ finance_mgmt_server/finance_mgmt_WebAPI/Authentication/IJwtTokenManager.cs
@@ -0,0 +1,9 @@
+﻿using finance_mgmt_Repository.Models;
+
+namespace finance_mgmt_WebAPI.Authentication
+{
+    public interface IJwtTokenManager
+    {
+        string GenerateJwt(UserInfo user);
+    }
+}
