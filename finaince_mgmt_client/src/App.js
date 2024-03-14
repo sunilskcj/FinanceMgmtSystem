9a47147744b33477cb564c932e8a9a44759e5fcc
sunilskcj <sunilskcj@gmail.com>
2024-03-14 12:54:06 +0530

    app

--- finaince_mgmt_client/src/App.js
+++ finaince_mgmt_client/src/App.js
@@ -2,7 +2,7 @@
 import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
 import './App.css';
 import Create_User from './Components/create-user/create-user';
-
+import { ToastContainer, toast } from 'react-toastify';
 import Home from './Components/Home/Home';
 import Footer from './Components/Footer/Footer';
 import LoginUser from './Components/LoginUser/LoginUser';
@@ -13,8 +13,10 @@ function App() {
   let nbsp = "\u00A0"
   return (
     <div className='App'>
+    <ToastContainer />
     <BrowserRouter>
       <Routes>
+        
         <Route exact path='/' element={ <LoginUser />} />
         <Route exact path='user/*' element={ <User />} />
       </Routes>
