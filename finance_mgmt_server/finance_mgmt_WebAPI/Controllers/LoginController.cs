7c303a74eb78f66f958219170e94b201ffb02191
sunilskcj <sunilskcj@gmail.com>
2024-03-16 12:54:06 +0530

    log  cont

--- finance_mgmt_server/finance_mgmt_WebAPI/Controllers/LoginController.cs
+++ finance_mgmt_server/finance_mgmt_WebAPI/Controllers/LoginController.cs
@@ -1,5 +1,7 @@
 ﻿using finance_mgmt_Repository.DataAccessLayer.Abstraction;
+using finance_mgmt_Repository.DataAccessLayer.Implementation;
 using finance_mgmt_Repository.Models;
+using finance_mgmt_WebAPI.Authentication;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,19 +11,26 @@ namespace finance_mgmt_WebAPI.Controllers
     [ApiController]
     public class LoginController : ControllerBase
     {
+        private IJwtTokenManager jwtTokenManager;
         private readonly ILogin login;
-        public LoginController(ILogin login)
+        public LoginController(ILogin login,IJwtTokenManager jwtTokenManager)
         {
             this.login = login;
+            this.jwtTokenManager = jwtTokenManager;
         }
         
         [HttpPost]
-        public IActionResult LoginUser(User cred)
+        public IActionResult LoginUser([FromBody]UserInfo cred)
         {
             try
             {
-                if (login.isUserExist(cred) > 0) return Ok(new { UserID = cred.UserId });
-                return NotFound();
+               
+                bool isAuthenticated = login.AuthenticateUser(cred);
+                if (!isAuthenticated)
+                    return this.NotFound("User is not authenticated");
+
+                var tokenValue = this.jwtTokenManager.GenerateJwt(cred);
+                return this.Ok(new { token = tokenValue });
             }
             catch (Exception ex)
             {
